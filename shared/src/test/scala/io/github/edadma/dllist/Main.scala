66fe30a893f3baabe1c8b07a081078400f64e655
Edward A Maxedon <edadma@gmail.com>
2025-04-23 00:23:50 -0400

    test

--- shared/src/test/scala/io/github/edadma/dllist/Main.scala
+++ shared/src/test/scala/io/github/edadma/dllist/Main.scala
@@ -2,8 +2,8 @@ package io.github.edadma.dllist
 
 object Main extends App {
 
-  val scalaList = List(1, 2, 3, 4, 5)
-  val dllist    = DLList(scalaList*)
+  val list   = List(1, 2, 3, 4, 5)
+  val dllist = DLList(list*)
 
   dllist.node(2).unlink
   println(dllist)
