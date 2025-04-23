92fa4f318c3a9d7db9f389ae7177b3495575bec2
Edward A Maxedon <edadma@gmail.com>
2025-04-23 00:20:21 -0400

    test

--- shared/src/test/scala/io/github/edadma/dllist/Main.scala
+++ shared/src/test/scala/io/github/edadma/dllist/Main.scala
@@ -4,17 +4,10 @@ import scala.compiletime.uninitialized
 
 object Main extends App {
 
-  val l = DLList(C(3), C(4))
+  val scalaList = List(1, 2, 3, 4, 5)
+  val dllist    = DLList(scalaList*)
 
-  println(l)
+  dllist.node(2).unlink
+  println(dllist)
 
-}
-
-case class C(asdf: Int) extends NodeRef {
-
-  private var n: DLList[C]#Node = uninitialized
-
-  def ref(node: DLList[Any]#Node): Unit = n = node.asInstanceOf[DLList[C]#Node]
-
-  override def toString: String = s"<$asdf, ${n.following}>"
 }
